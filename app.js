95ca9b2991fb717b3b093f07f654b8fd82112f53
cano316 <christiancano316@gmail.com>
2022-06-28 14:40:09 -0700

    removed require dotenv

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const axios = require('axios').default;
-require('dotenv').config();
 // Serve static files
 app.use(express.static("public"))
 // Parser
