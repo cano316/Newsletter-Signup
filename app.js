7534f35f68e451a5004727f388230f5a51c0f09f
cano316 <christiancano316@gmail.com>
2022-06-28 15:52:53 -0700

    install dotenv to read environment variables

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const axios = require('axios').default;
+require('dotenv').config();
 // Serve static files
 app.use(express.static("public"))
 // Parser
